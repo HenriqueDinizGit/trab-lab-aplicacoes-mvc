f9fd05550697a361f004a698bacf25d002f98290
HenriqueDinizGit <hsdiniz01@gmail.com>
2024-05-04 16:04:26 -0300

    etapa 4

---
+++ home-broker/src/main/java/com/trabmvc/homebroker/services/ProdutoService.java
@@ -0,0 +1,24 @@
+package com.trabmvc.homebroker.services;
+
+import com.trabmvc.homebroker.models.Produto;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+
+    public List<Produto> listarProdutos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listarProdutos'");
+    }
+
+    public Produto adicionarProduto(Produto produto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'adicionarProduto'");
+    }
+
+    // Adicione outros métodos conforme necessário
+}
