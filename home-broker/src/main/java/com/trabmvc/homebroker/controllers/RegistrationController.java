72c1074eba866af427f9fdce46da48b947a3a09a
Klaus-Teles <klausteled@gmail.com>
2024-05-12 19:13:14 -0300

    Registrar novo usuario

---
+++ home-broker/src/main/java/com/trabmvc/homebroker/controllers/RegistrationController.java
@@ -0,0 +1,36 @@
+package com.trabmvc.homebroker.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.trabmvc.homebroker.models.User;
+import com.trabmvc.homebroker.repositories.UserRepository;
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/register")
+    public String showRegistrationForm() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUserAccount(@RequestParam("username") String username,
+                                      @RequestParam("password") String password) {
+        User newUser = new User();
+        newUser.setUsername(username);
+        newUser.setPassword(passwordEncoder.encode(password));
+        userRepository.save(newUser);
+        return "redirect:/login";
+    }
+}
